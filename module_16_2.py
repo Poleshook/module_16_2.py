c4d71d54edaada7c4fca881119ee4e7a366d8f0b
Poleshook <poleshook@mail.ru>
2025-01-15 22:28:54 +0500

    module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,  Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -14,11 +15,12 @@ async def get_admin():
 
 
 @app.get("/user/{user_id}")
-async def get_user(user_id: int = Path(ge=1, le=100, description='Enter User ID', example='1')):
+async def get_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]):
     return (f"Вы вошли как пользователь № {user_id}")
 
 
 @app.get("/user/{username}/{age}")
-async def get_user(username: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
-                   age: int = Path(ge=18, le=120, description='Enter age', example='24')):
+async def get_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+                                                 example='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
     return (f"Информация о пользователе. Имя: {username}, Возраст: {age}.")
